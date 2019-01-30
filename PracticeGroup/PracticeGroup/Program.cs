2695c98d0cb7afa976a73d011bbdf80153f61e4e
Mohit Dedhe <mohitdedhe666@gmail.com>
2019-01-29 19:55:16 -0500

    group project with class

---
+++ PracticeGroup/PracticeGroup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++
@@ -1,8 +0,0 @@
-# DISGroupRepo
-This repository is used for the group assignment for DIS class.
-
-Team members are 
-
-1. Keshav
-2. Sagar Billore
-3. Mohit Dedhe
